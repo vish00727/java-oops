02259d37b172a50822780f9210e3dccb9e79ede1
vish00727 <149477636+vish00727@users.noreply.github.com>
2024-01-15 15:00:31 +0530

    Update Charge.java

--- Charge.java
+++ Charge.java
@@ -6,10 +6,13 @@ public  class Charge {
         int temp=0;
         int [] arr= {23,43,65,87,23,44,34,33};
         for (int i=0; i<arr.length; i++){
+            for(int j=0; j<i; j++){
+                
             if (arr[i]> arr[i+1]){
                 temp=arr[i];
                 arr[i]=arr[i+1];
                 arr[i+1]=temp;
+                  }
             }
             System.out.print(arr[i]);
         }

1d0570e003021fb8f94ce4b6e92de811a6148408
vish00727 <149477636+vish00727@users.noreply.github.com>
2024-01-27 15:21:20 +0530

    Update OverridingProtect.java
    change

--- OverridingProtect.java
+++ OverridingProtect.java
@@ -1,1 +1,45 @@
+package OOps;
+
+public class Private23 {
+
+    void m1(){
+        System.out.println("Private 23 m1");
+
+        m2();
+    }
+
+    private void m2(){
+        System.out.println("Private 23 m2");
+    }
+}
+
+class XYz extends Private23{
+
+
+    @java.lang.Override
+    void m1() {
+        System.out.println("m1 xyz");
+        m2();
+    }
+
+    private void m2(){
+        System.out.println("m2 xyz");
+    }
+}
+
+
+
+class ttt{
+
+
+    public static void main(String[] args) {
+        Private23 bb= new Private23();
+//        bb.m1();
+        Private23 obj=new XYz();
+        obj.m1();
+
+
+    }
+}
+
 
